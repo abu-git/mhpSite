69d45da44b67d7f8e13e1f7861765ac159cd3e94
abu-git <abu.sammie@gmail.com>
2018-12-05 19:18:45 +0200

    3rd commit: Server works. Static extends works

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, JourneyPageView, TapesPageView, PhotosPageView
 
 urlpatterns = [
 	path('', HomePageView.as_view(), name='mhp-home'),
+	path('journey/', JourneyPageView.as_view(), name='mhp-journey'),
+	path('tapes/', TapesPageView.as_view(), name='mhp-tapes'),
+	path('photos/', PhotosPageView.as_view(), name='mhp-photos'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -3,4 +3,14 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class HomePageView(TemplateView):
-	template_name = 'base.html'
+	template_name = 'home.html'
+
+class JourneyPageView(TemplateView):
+	template_name = "journey.html"
+
+class TapesPageView(TemplateView):
+	template_name= "mhp-tapes.html"
+
+class PhotosPageView(TemplateView):
+	template_name = 'mhp-photos.html'
+
